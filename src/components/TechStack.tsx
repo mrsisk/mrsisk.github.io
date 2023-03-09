f2a07ddade823e7ec7c283e7ade9b077b011ec5e
mrsisk <siskmusik@gmail.com>
2023-03-09 13:48:03 +0200

    tech stack card

--- src/components/TechStack.tsx
+++ src/components/TechStack.tsx
@@ -1,14 +1,14 @@
 
 export default function TechStack() {
   return (
-    <div className="bg-gray-300 py-12 sm:py-12">
+    <div className="bg-gray- py-12 sm:py-12">
       <div className="mx-auto max-w-7xl px-6 lg:px-8">
-        <h2 className="text-center text-2xl md:text-4xl font-semibold leading-8 text-gray-900">
+        <h2 className="text-center text-2xl md:text-4xl font-semibold leading-8 text-white">
           Tech stack
         </h2>
         <div className="mx-auto mt-10 grid max-w-lg grid-cols-4 items-center gap-x-8 gap-y-10 sm:max-w-xl sm:grid-cols-6 sm:gap-x-10 lg:mx-0 lg:max-w-none lg:grid-cols-5">
           <img
-            className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
+            className="col-span-2 max-h-12 w-full object-contain lg:col-span-1 bg-gray-200 rounded-xl"
             src="https://www.vectorlogo.zone/logos/kotlinlang/kotlinlang-ar21.svg"
             alt="Reform"
             width="158"
@@ -16,7 +16,7 @@ export default function TechStack() {
           ></img>
 
           <img
-            className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
+            className="col-span-2 max-h-12 w-full object-contain lg:col-span-1 bg-gray-200 rounded-xl"
             src="https://www.vectorlogo.zone/logos/java/java-ar21.svg"
             alt="Reform"
             width="158"
@@ -24,7 +24,7 @@ export default function TechStack() {
           ></img>
 
           <img
-            className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
+            className="col-span-2 max-h-12 w-full object-contain lg:col-span-1 bg-gray-200 rounded-xl"
             src="https://www.vectorlogo.zone/logos/nodejs/nodejs-ar21.svg"
             alt="Transistor"
             width="158"
@@ -32,14 +32,14 @@ export default function TechStack() {
           ></img>
 
           <img
-            className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
+            className="col-span-2 max-h-12 w-full object-contain lg:col-span-1 bg-gray-200 rounded-xl"
             src="https://www.vectorlogo.zone/logos/typescriptlang/typescriptlang-ar21.svg"
             alt="Transistor"
             width="158"
             height="48"
           ></img>
           <img
-            className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
+            className="col-span-2 max-h-12 w-full object-contain lg:col-span-1 bg-gray-200 rounded-xl"
             src="https://www.vectorlogo.zone/logos/elixir-lang/elixir-lang-ar21.svg"
             alt="Transistor"
             width="158"
@@ -47,7 +47,7 @@ export default function TechStack() {
           ></img>
 
           <img
-            className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
+            className="col-span-2 max-h-12 w-full object-contain lg:col-span-1 bg-gray-200 rounded-xl"
             src="https://www.vectorlogo.zone/logos/docker/docker-ar21.svg"
             alt="Transistor"
             width="158"
@@ -55,7 +55,7 @@ export default function TechStack() {
           ></img>
 
           <img
-            className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
+            className="col-span-2 max-h-12 w-full object-contain lg:col-span-1 bg-gray-200 rounded-xl"
             src="https://www.vectorlogo.zone/logos/gradle/gradle-ar21.svg"
             alt="Transistor"
             width="158"
@@ -63,7 +63,7 @@ export default function TechStack() {
           ></img>
 
           <img
-            className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
+            className="col-span-2 max-h-12 w-full object-contain lg:col-span-1 bg-gray-200 rounded-xl"
             src="https://www.vectorlogo.zone/logos/git-scm/git-scm-ar21.svg"
             alt="Transistor"
             width="158"
@@ -71,7 +71,7 @@ export default function TechStack() {
           ></img>
 
           <img
-            className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
+            className="col-span-2 max-h-12 w-full object-contain lg:col-span-1 bg-gray-200 rounded-xl"
             src="https://www.vectorlogo.zone/logos/springio/springio-ar21.svg"
             alt="Transistor"
             width="158"
@@ -79,7 +79,7 @@ export default function TechStack() {
           ></img>
 
           <img
-            className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
+            className="col-span-2 max-h-12 w-full object-contain lg:col-span-1 bg-gray-200 rounded-xl"
             src="https://blixtdev.com/content/images/size/w2000/2023/02/1WvDl2WlPs7cR8TTBvrjpyw.png"
             alt="Transistor"
             width="158"
@@ -87,14 +87,14 @@ export default function TechStack() {
           ></img>
 
           <img
-            className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
+            className="col-span-2 max-h-12 w-full object-contain lg:col-span-1 bg-gray-200 rounded-xl"
             src="https://www.vectorlogo.zone/logos/reactjs/reactjs-ar21.svg"
             alt="Transistor"
             width="158"
             height="48"
           ></img>
           <img
-            className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
+            className="col-span-2 max-h-12 w-full object-contain lg:col-span-1 bg-gray-200 rounded-xl"
             src="https://www.vectorlogo.zone/logos/vuejs/vuejs-ar21.svg"
             alt="Transistor"
             width="158"
@@ -102,14 +102,14 @@ export default function TechStack() {
           ></img>
 
           <img
-            className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
+            className="col-span-2 max-h-12 w-full object-contain lg:col-span-1 bg-gray-200 rounded-xl"
             src="https://www.vectorlogo.zone/logos/tailwindcss/tailwindcss-ar21.svg"
             alt="Transistor"
             width="158"
             height="48"
           ></img>
           <img
-            className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
+            className="col-span-2 max-h-12 w-full object-contain lg:col-span-1 bg-gray-200 rounded-xl"
             src="https://www.vectorlogo.zone/logos/flutterio/flutterio-ar21.svg"
             alt="Transistor"
             width="158"
@@ -117,14 +117,14 @@ export default function TechStack() {
           ></img>
 
           <img
-            className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
+            className="col-span-2 max-h-12 w-full object-contain lg:col-span-1 bg-gray-200 rounded-xl"
             src="https://www.vectorlogo.zone/logos/nestjs/nestjs-ar21.svg"
             alt="Transistor"
             width="158"
             height="48"
           ></img>
           <img
-            className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
+            className="col-span-2 max-h-12 w-full object-contain lg:col-span-1 bg-gray-200 rounded-xl"
             src="https://www.vectorlogo.zone/logos/expressjs/expressjs-ar21.svg"
             alt="Transistor"
             width="158"
@@ -132,7 +132,7 @@ export default function TechStack() {
           ></img>
 
           <img
-            className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
+            className="col-span-2 max-h-12 w-full object-contain lg:col-span-1 bg-gray-200 rounded-xl"
             src="https://www.vectorlogo.zone/logos/amazon_aws/amazon_aws-ar21.svg"
             alt="Transistor"
             width="158"
@@ -140,7 +140,7 @@ export default function TechStack() {
           ></img>
 
           <img
-            className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
+            className="col-span-2 max-h-12 w-full object-contain lg:col-span-1 bg-gray-200 rounded-xl"
             src="https://www.vectorlogo.zone/logos/google_cloud/google_cloud-ar21.svg"
             alt="Transistor"
             width="158"
@@ -148,7 +148,7 @@ export default function TechStack() {
           ></img>
 
           <img
-            className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
+            className="col-span-2 max-h-12 w-full object-contain lg:col-span-1 bg-gray-200 rounded-xl"
             src="https://www.vectorlogo.zone/logos/mongodb/mongodb-ar21.svg"
             alt="Transistor"
             width="158"
@@ -156,14 +156,14 @@ export default function TechStack() {
           ></img>
 
           <img
-            className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
+            className="col-span-2 max-h-12 w-full object-contain lg:col-span-1 bg-gray-200 rounded-xl"
             src="https://www.vectorlogo.zone/logos/postgresql/postgresql-ar21.svg"
             alt="Transistor"
             width="158"
             height="48"
           ></img>
         </div>
-        <h2 className="text-center text-lg font-semibold leading-8 text-gray-900 mt-10">
+        <h2 className="text-center text-lg font-semibold leading-8 text-white mt-10">
           And many more...
         </h2>
       </div>
